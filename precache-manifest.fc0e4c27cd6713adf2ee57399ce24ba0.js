fbf6c206592666fa2e23c86012d934af430a33b4
garnere1 <emilygarner24@gmail.com>
2023-06-14 13:54:52 -0400

    Updates

---
+++ precache-manifest.fc0e4c27cd6713adf2ee57399ce24ba0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab873f6efcdacaaec8bb",
+    "url": "/static/css/main.269bdf17.chunk.css"
+  },
+  {
+    "revision": "ab873f6efcdacaaec8bb",
+    "url": "/static/js/main.ab873f6e.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "1901ec62d8a6081be676",
+    "url": "/static/js/2.1901ec62.chunk.js"
+  },
+  {
+    "revision": "72cba3a7707ecab0fd5b",
+    "url": "/static/js/runtime~main.72cba3a7.js"
+  },
+  {
+    "revision": "158059a3d84125ad29b210ffd8101b64",
+    "url": "/static/media/computer.158059a3.png"
+  },
+  {
+    "revision": "7b2f6b7bd33a7c8b5b66a8fcecf1bb53",
+    "url": "/static/media/aws.7b2f6b7b.png"
+  },
+  {
+    "revision": "574a86fadbe6f4d30ccc1ecfe763f0de",
+    "url": "/static/media/headshot.574a86fa.jpeg"
+  },
+  {
+    "revision": "f6701a5515bcf538c18df18303072500",
+    "url": "/static/media/purdue.f6701a55.png"
+  },
+  {
+    "revision": "738727bf910370020e4f1b52c75e054f",
+    "url": "/index.html"
+  }
+];
--- public/CNAME
+++
@@ -1,1 +0,0 @@
-emilyanngarner.com
