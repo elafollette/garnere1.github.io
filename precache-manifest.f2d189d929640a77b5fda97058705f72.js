73ce029563604877a80049d306e3c0a8774aac0e
garnere1 <emilygarner24@gmail.com>
2023-06-15 20:33:56 -0400

    Updates

---
+++ precache-manifest.f2d189d929640a77b5fda97058705f72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a12f4bc8298738417fb5",
+    "url": "/static/css/main.b13bef15.chunk.css"
+  },
+  {
+    "revision": "a12f4bc8298738417fb5",
+    "url": "/static/js/main.a12f4bc8.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "1901ec62d8a6081be676",
+    "url": "/static/js/2.1901ec62.chunk.js"
+  },
+  {
+    "revision": "72cba3a7707ecab0fd5b",
+    "url": "/static/js/runtime~main.72cba3a7.js"
+  },
+  {
+    "revision": "158059a3d84125ad29b210ffd8101b64",
+    "url": "/static/media/computer.158059a3.png"
+  },
+  {
+    "revision": "7b2f6b7bd33a7c8b5b66a8fcecf1bb53",
+    "url": "/static/media/aws.7b2f6b7b.png"
+  },
+  {
+    "revision": "574a86fadbe6f4d30ccc1ecfe763f0de",
+    "url": "/static/media/headshot.574a86fa.jpeg"
+  },
+  {
+    "revision": "f6701a5515bcf538c18df18303072500",
+    "url": "/static/media/purdue.f6701a55.png"
+  },
+  {
+    "revision": "f3a1cd0460896e8c08340f102a9133f7",
+    "url": "/index.html"
+  }
+];
